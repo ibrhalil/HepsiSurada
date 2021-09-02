d90089ace94e868a0298e6fc5e36867201de05ca
Halil <ibrhalilaydn@gmail.com>
2021-09-02 15:25:47 +0300

    test catagory, product  ve customer eklemesi yapıldı

--- src/main/java/com/jia/HepsiSurada/controller/TestController.java
+++ src/main/java/com/jia/HepsiSurada/controller/TestController.java
@@ -4,6 +4,7 @@ import com.jia.HepsiSurada.core.DataResult;
 import com.jia.HepsiSurada.dto.CategoryDTO;
 import com.jia.HepsiSurada.dto.CustomerDTO;
 import com.jia.HepsiSurada.dto.ProductDTO;
+import com.jia.HepsiSurada.model.Adress;
 import com.jia.HepsiSurada.model.Category;
 import com.jia.HepsiSurada.model.Customer;
 import com.jia.HepsiSurada.service.CategoryService;
@@ -13,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController("/api/test")
@@ -27,8 +30,17 @@ public class TestController
     @Autowired
     private CustomerService customerService;
 
+    private DataResult<List<Category>> categoryList;
 
-    @GetMapping("/category")
+    @GetMapping("/add")
+    public void addAll()
+    {
+        ekleCategory();
+        ekleProduct();
+        ekleCustomer();
+        ekleCustomerAddAddress();
+    }
+
     public void ekleCategory()
     {
         categoryService.addCategory(new CategoryDTO("bilgisayar"));
@@ -36,23 +48,42 @@ public class TestController
         categoryService.addCategory(new CategoryDTO("giyim"));
         categoryService.addCategory(new CategoryDTO("mutfak"));
         categoryService.addCategory(new CategoryDTO("oyun"));
+
     }
 
-    @GetMapping("/product")
     public void  ekleProduct()
     {
-        DataResult<List<Category>> categoryList = categoryService.getCategoryList();
-        productService.addProduct(new ProductDTO(110.5,"Rokstar","bu bir oyundur","GTA5",""));
-        productService.addProduct(new ProductDTO(210.5,"EA GAMES","bu bir oyundur","MostWanted",""));
-        productService.addProduct(new ProductDTO(50.5,"EA SPORTS","bu bir oyundur","FIFA21",""));
+
+        categoryList = categoryService.getCategoryList();
+
+        Category oyun = categoryList.getData().get(4);
+        productService.addProduct(new ProductDTO(110.5,"Rokstar","bu bir oyundur","GTA5",oyun.getId().toString()));
+        productService.addProduct(new ProductDTO(210.5,"EA GAMES","bu bir oyundur","MostWanted",oyun.getId().toString()));
+        productService.addProduct(new ProductDTO(50.5,"EA SPORTS","bu bir oyundur","FIFA21",oyun.getId().toString()));
     }
 
-    @GetMapping("/customer")
-    public void ekleCustomer(){
-        DataResult<List<Customer>> customerList = customerService.getListCustomer();
+    public void ekleCustomer()
+    {
+
         customerService.save(new CustomerDTO("glo","arda","sen","bizimarda@hotmail.com","123123"));
         customerService.save(new CustomerDTO("oga","halil","ip","serseristyle@hotmail.com","123123"));
         customerService.save(new CustomerDTO("qwe","emre","öz","serbestdeli@hotmail.com","123123"));
         customerService.save(new CustomerDTO("asd","mert","tek","kirikkafa@hotmail.com","123123"));
+    }
+
+    public void ekleCustomerAddAddress()
+    {
+
+        List<Adress> adresses1 = new ArrayList<>(Arrays.asList(new Adress("türkiye","istanbul","mahmutbey"),new Adress("türkiye","ankara","mamak")));
+        customerService.updateAdressByNickName("glo",adresses1);
+
+        List<Adress> adresses2 = new ArrayList<>(Arrays.asList(new Adress("türkiye","istanbul","maslak")));
+        customerService.updateAdressByNickName("oga",adresses2);
+
+        List<Adress> adresses3 = new ArrayList<>(Arrays.asList(new Adress("abd","califirnia","")));
+        customerService.updateAdressByNickName("qwe",adresses3);
+
+        List<Adress> adresses4 = new ArrayList<>(Arrays.asList(new Adress("türkiye","istanbul","ümraniye"),new Adress("türkiye","istanbul","pendik")));
+        customerService.updateAdressByNickName("asd",adresses4);
     }
 }
